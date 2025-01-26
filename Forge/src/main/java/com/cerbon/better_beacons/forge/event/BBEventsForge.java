62cef5a336edd59282c9f480c475c8fc6fc7d6ee
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-01-26 11:02:21 -0300

    Remove CraftingHelper.register

--- Forge/src/main/java/com/cerbon/better_beacons/forge/event/BBEventsForge.java
+++ Forge/src/main/java/com/cerbon/better_beacons/forge/event/BBEventsForge.java
@@ -1,7 +1,6 @@
 package com.cerbon.better_beacons.forge.event;
 
 import com.cerbon.better_beacons.advancement.BBCriteriaTriggers;
-import com.cerbon.better_beacons.forge.advancement.condition.custom.IsConfigEnabledCondition;
 import com.cerbon.better_beacons.util.BBConstants;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -13,8 +12,6 @@ public class BBEventsForge {
 
     @SubscribeEvent
     public static void onCommonSetupEvent(FMLCommonSetupEvent event) {
-        CraftingHelper.register(IsConfigEnabledCondition.Serializer.INSTANCE);
-
         CriteriaTriggers.register("better_beacons:redirect_beacon", BBCriteriaTriggers.REDIRECT_BEACON);
         CriteriaTriggers.register("better_beacons:invisible_beam", BBCriteriaTriggers.INVISIBLE_BEAM);
         CriteriaTriggers.register("better_beacons:increase_effects_strength", BBCriteriaTriggers.INCREASE_EFFECTS_STRENGTH);
